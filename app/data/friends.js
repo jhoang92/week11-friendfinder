1c40d6be11a7a4735dc13e64c87c379897f5d54c
jhoang92 <jhhoang992@gmail.com>
2018-05-12 09:32:25 -0600

    added working routes

---
+++ app/data/friends.js
@@ -0,0 +1,68 @@
+var friends = [
+    {
+        "name": "Karina",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            3,
+            2,
+            6,
+            9,
+            8,
+            9,
+            9
+        ]
+    },
+    {
+        "name": "Miriam",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            3,
+            2,
+            6,
+            9,
+            8,
+            9,
+            9
+        ]
+    },
+    {
+        "name": "Jason",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            3,
+            2,
+            6,
+            9,
+            8,
+            9,
+            9
+        ]
+    },
+    {
+        "name": "Nate",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            3,
+            2,
+            6,
+            9,
+            8,
+            9,
+            9
+        ]
+    }
+]
+
+module.exports = friends;
